2867b0ea6e2985eb7cfb6e0f973db34ec76ff315
ksnx3684 <ksnx3684@naver.com>
2022-06-09 16:37:02 +0900

    Chore : WebErrorController 주석 해제

--- src/main/java/com/project/mango/util/WebErrorController.java
+++ src/main/java/com/project/mango/util/WebErrorController.java
@@ -1,28 +1,35 @@
-/*
- * package com.project.mango.util;
- * 
- * import javax.servlet.RequestDispatcher; import
- * javax.servlet.http.HttpServletRequest;
- * 
- * import org.springframework.boot.web.servlet.error.ErrorController; import
- * org.springframework.http.HttpStatus; import
- * org.springframework.stereotype.Controller; import
- * org.springframework.web.bind.annotation.GetMapping;
- * 
- * @Controller public class WebErrorController implements ErrorController {
- * 
- * public String getErrorPath() { return null; }
- * 
- * @GetMapping("/error") public String handleError(HttpServletRequest request) {
- * Object status = request.getAttribute(RequestDispatcher.ERROR_STATUS_CODE);
- * 
- * if(status != null){ int statusCode = Integer.valueOf(status.toString());
- * 
- * if(statusCode == HttpStatus.NOT_FOUND.value()) { return "error/404error"; }
- * else { return "error/error"; } }
- * 
- * return "error/error"; }
- * 
- * }
- * 
- */
+package com.project.mango.util;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.http.HttpServletRequest;
+
+import org.springframework.boot.web.servlet.error.ErrorController;
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class WebErrorController implements ErrorController {
+	
+	public String getErrorPath() {
+        return null;
+    }
+
+    @GetMapping("/error")
+    public String handleError(HttpServletRequest request) {
+        Object status = request.getAttribute(RequestDispatcher.ERROR_STATUS_CODE);
+
+        if(status != null){
+            int statusCode = Integer.valueOf(status.toString());
+
+            if(statusCode == HttpStatus.NOT_FOUND.value()) {
+                return "error/404error";
+            } else {
+                return "error/error";
+            }
+        }
+
+        return "error/error";
+    }
+
+}
